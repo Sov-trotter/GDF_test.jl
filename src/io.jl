444b4a4ea087c1299ff60447ea0fe4295968a996
ARSH SHARMA <184517@nith.ac.in>
2020-05-12 22:58:14 +0530

    Tables interface working, output a DataFrame

--- src/io.jl
+++ src/io.jl
@@ -3,8 +3,12 @@ function gdf_from_geojson(parsed_geojson)
     feat = parsed_geojson.features
 
     tbl = GeoTable(feat)
-    return tbl
+    
+    return DataFrame(tbl)
 end
+
+
+# function read(parsed_geojson)
 
 # function gdf_from_shp()
 
--- src/tables.jl
+++ src/tables.jl
@@ -4,7 +4,7 @@ end
 
 function GeoTable(features::T) where T<:Array
     geom = features[1].geometry
-    if(geom == nothing)
+    if(geom === nothing)
         error("Not a valid geojson")
     end
     GeoTable{T}(features)
@@ -20,17 +20,18 @@ Base.IteratorEltype(::Type{<:GeoTable}) = Base.HasEltype()
 
 # read only AbstractVector
 Base.size(gt::GeoTable) = length(gt.features)
-Base.getindex(gt::GeoTable, i) = GeoTableRow(Row(gt)[i])
+Base.getindex(gt::GeoTable, i) = iterate((gt.features), i)
 Base.IndexStyle(::Type{<:GeoTable}) = IndexLinear()
 
 struct GeoTableRow
-    Row
-    feature_number::Int
+    UUID   # TODO : fetch property names paticular to the geojson
+    FID
+    Geometry
 end
 
-function Tables.schema(features)
+function Tables.schema(features::Array)
 
-    names_fields = Tuple(keys(features[1].properties))
+    names_fields = (keys(features[1].properties))
     geom_names = ("Geometry")
     names = (names_fields..., geom_names)
     
@@ -44,29 +45,17 @@ end
 
 
 
-# Tables.columnaccess(gt::GeoTable) = true
-# Tables.columns(gt::GeoTable) = gt
-
-# Tables.getcolumn(gt::GeoTable, i::Int) = 
-# Tables.getcolumn(gt::GeoTable, nm::Symbol) = iterate(gt.features)
-
-
-
-
-# Base.getindex(gtr::GeoTableRow, i) = gtr.Row[i]
-# Base.IndexStyle(::Type{<:GeoTableRow}) = IndexLinear()
-# Base.getindex(gt::GeoTable, i) = (gt.features)[i]
-# Base.length(gt::GeoTable) = (length(gt.features))
-
-
-
-
 function Base.iterate(gt::GeoTable, st = 1)
     features = gt.features
 
     d = Dict{String, Vector}()
 
-    for i in String.(Tables.schema(gt.features).names)
+    geom = ""
+    prop_1 = ""
+    prop_2 = ""
+
+    namesz =  String.(Tables.schema(gt.features).names)
+    for i in namesz
         if(i == "Geometry")
             d[i] = []
         else
@@ -78,47 +67,19 @@ function Base.iterate(gt::GeoTable, st = 1)
     for (k, v) in pairs(d)
         if k == "Geometry"
             val = features[st].geometry
-        else 
+            geom = val
+        elseif k == namesz[1]
             val = features[st].properties[k]
+            prop_1 = val
+        elseif k == namesz[2]
+        # else 
+            val = features[st].properties[k]
+            prop_2 = val
         end
-        push!(v, val)
+        
+        
     end
     
-    
-    keys_tup = ()
-    for _key in keys(d)
-        keys_tup = (keys_tup..., Symbol(_key))
-    end
-    
-    namez = ()
-    for k in keys(d)
-        namez = (namez..., Symbol(k))
-    end
-    
-    value = Tuple(values(d))
-
-    Row = NamedTuple{namez}(value)
-    return GeoTableRow(Row, st), st + 1
+    return GeoTableRow(prop_1, prop_2, geom), st + 1
 end
-
-# required `Tables.AbstractRow` interface methods
-# getcolumn(gtr::GeoTableRow, i::Int) = getindex(gtr.Row, i)
-# getcolumn(gtr::GeoTableRow, nm::Symbol) = getindex(gtr.Row, nm)
-# columnnames(gtr::GeoTableRow) = String.(keys(gtr.Row))
-
-# function Base.show(io::IO, gt::GeoTable, gtr::GeoTableRow)
-#     println(io, "$(length(gtr.Row))")
-# end
-
-
-
-
-
-
-
-
-
-
-
-
 
